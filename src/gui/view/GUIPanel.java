6ac4d297f8eed749d51662862145a745eeb2e5e6
ForrestThacker <fyruskasai@gmail.com>
2015-11-12 11:31:15 -0700

    Added to repo.

--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -7,6 +7,11 @@ import javax.swing.SpringLayout;
 import gui.controller.GUIAppController;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JSlider;
+import java.awt.Color;
+import javax.swing.UIManager;
+import java.awt.Font;
+import javax.swing.JRadioButton;
 
 
 /**
@@ -23,10 +28,20 @@ public class GUIPanel extends JPanel
 	
 	public GUIPanel(GUIAppController baseController)
 	{
+		setBackground(UIManager.getColor("textHighlight"));
 		this.baseController = baseController;
 		baseLayout = new SpringLayout();
 		firstButton = new JButton("Do not click the button pls");
+		firstButton.setForeground(new Color(0, 0, 0));
+		firstButton.setFont(new Font("Comic Sans MS", Font.PLAIN, 13));
+		firstButton.setBackground(new Color(255, 192, 203));
+		baseLayout.putConstraint(SpringLayout.WEST, firstButton, 116, SpringLayout.WEST, this);
 		firstTextField = new JTextField("You can put words in this box");
+		firstTextField.setBackground(new Color(250, 128, 114));
+		firstTextField.setFont(new Font("Comic Sans MS", Font.PLAIN, 13));
+		baseLayout.putConstraint(SpringLayout.NORTH, firstTextField, 24, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, firstTextField, 127, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.EAST, firstTextField, 0, SpringLayout.EAST, firstButton);
 	
 		setupPanel();
 		setupLayout();
@@ -42,6 +57,17 @@ public class GUIPanel extends JPanel
 		this.setLayout(baseLayout);		//Needs to be the first line of the setupPanel helper method.
 		this.add(firstButton);
 		this.add(firstTextField);
+		
+		JSlider slider = new JSlider();
+		baseLayout.putConstraint(SpringLayout.SOUTH, firstButton, -39, SpringLayout.NORTH, slider);
+		baseLayout.putConstraint(SpringLayout.NORTH, slider, 172, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.EAST, slider, -129, SpringLayout.EAST, this);
+		add(slider);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Wow so code much developing");
+		baseLayout.putConstraint(SpringLayout.WEST, rdbtnNewRadioButton, 24, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, rdbtnNewRadioButton, -26, SpringLayout.SOUTH, this);
+		add(rdbtnNewRadioButton);
 	}
 	
 	/**
@@ -49,11 +75,7 @@ public class GUIPanel extends JPanel
 	 */
 	
 	private void setupLayout()
-	{   baseLayout.putConstraint(SpringLayout.NORTH, firstTextField, 65, SpringLayout.SOUTH, firstButton);
-		baseLayout.putConstraint(SpringLayout.WEST, firstTextField, 0, SpringLayout.WEST, firstButton);
-		baseLayout.putConstraint(SpringLayout.EAST, firstTextField, 320, SpringLayout.WEST, this);
-		baseLayout.putConstraint(SpringLayout.WEST, firstButton, 117, SpringLayout.WEST, this);
-		baseLayout.putConstraint(SpringLayout.SOUTH, firstButton, -181, SpringLayout.SOUTH, this);
+	{
 	}
 	
 	private void setupListeners()
